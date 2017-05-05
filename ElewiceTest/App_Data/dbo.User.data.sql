1e521acf8da3537a561a3c3d80b6ccf44a520229
Ellozoid <ellozoid@gmail.com>
2017-05-06 03:06:40 +0400

    Database clean, users sql added

---
+++ ElewiceTest/App_Data/dbo.User.data.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT [dbo].[User] ON
+INSERT INTO [dbo].[User] ([Id], [UserName], [PasswordHash]) VALUES (1, N'ellozoid', N'AD8F5sQtcrl/vDU7nyeCYGjbd34Sm9HM4Lze6Tc14NzYMANBKF9yH2ARTUKzH3C/PQ==')
+INSERT INTO [dbo].[User] ([Id], [UserName], [PasswordHash]) VALUES (2, N'kek', N'ANYOCKAhtFT/7IqocPje0QbhhhihrWK4bu6jx6RUl5DNNSjOnAT28TbWLbaWb6zosw==')
+INSERT INTO [dbo].[User] ([Id], [UserName], [PasswordHash]) VALUES (5, N'Qwerty1', N'AFxT9oXstx2byYzUx9Ax1qHgnaw610/yNhF4bl8FwXvMdibGbl31VhuSk7ZhoerFlA==')
+INSERT INTO [dbo].[User] ([Id], [UserName], [PasswordHash]) VALUES (6, N'Qazxsw', N'AJYbFlrh6h2hWppzahrZ3cczWWI4Q2/1czDbQA5Omc2KfQG7G8QJ6PuZ/XKZHvQv3Q==')
+INSERT INTO [dbo].[User] ([Id], [UserName], [PasswordHash]) VALUES (7, N'wsxzaq', N'ABqi9zdfs9mc86DJ8yx6kxBma9pRF6xCm++hFzULC723EZmXYSO8AmdDbCpdoen0dA==')
+SET IDENTITY_INSERT [dbo].[User] OFF
